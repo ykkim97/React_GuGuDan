11c3a219e2bbc98c430a511bbe522d9102ab995e
YoungGwon Kim <97ykkim@naver.com>
2022-04-17 20:40:00 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [firstNumber, setFirstNumber] = useState(Math.ceil(Math.random() * 9));
+  const [secondNumber, setSecondNumber] = useState(Math.ceil(Math.random() * 9));
+  const [value, setValue] = useState("");
+  const [result, setResult] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (value === "") {
+      setResult('숫자를 입력해주세요!');
+    } else if(parseInt(value) === (firstNumber * secondNumber)) {
+      setResult('정답!!');
+      setFirstNumber(Math.ceil(Math.random() * 9));
+      setSecondNumber(Math.ceil(Math.random() * 9));
+      setValue("");
+    } else {
+      setResult('땡! 다시 입력해보세요!');
+      setValue("")
+    }
+  }
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <h1>{firstNumber} X {secondNumber} 은?</h1>
+        <form onSubmit={onSubmit} className="form">
+          <span>
+            <input 
+              className='input'
+              type="text"
+              value={value} 
+              onChange={onChange}
+            />
+          </span>
+          <button className='btn'>입력!</button>
+          <div className='result'>{result}</div>
+        </form>
+      </div>
     </div>
   );
 }
